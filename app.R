7ca9eb08be7c6242cff365c637462c17335df8b6
KepaAmigoTECHFriendly <58561058+KepaAmigoTECHFriendly@users.noreply.github.com>
2020-09-17 09:55:23 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -40,26 +40,6 @@ library(shinybusy)
 source("llamada_api_thb_borme.R")
 
 
-inactivity <- "function idleTimer() {
-  var t = setTimeout(logout, 500000);
-  window.onmousemove = resetTimer; // catches mouse movements
-  window.onmousedown = resetTimer; // catches mouse movements
-  window.onclick = resetTimer;     // catches mouse clicks
-  window.onscroll = resetTimer;    // catches scrolling
-  window.onkeypress = resetTimer;  //catches keyboard actions
-
-  function logout() {
-    window.close();  //close the window
-  }
-
-  function resetTimer() {
-    clearTimeout(t);
-    t = setTimeout(logout, 500000);  // time is in milliseconds (1000 is 1 second)
-  }
-}
-idleTimer();"
-
-
 # MUNICIPIOS
 # ------------------------
 
@@ -228,8 +208,6 @@ df_expediente_trabajo$fecha <- rep("2020-08-01",nrow(df_expediente_trabajo))
 ui <- fluidPage(style = "width: 100%; height: 100%;",
                 
                 #use_busy_spinner(spin = "fading-circle"),
-                
-                tags$script(inactivity),
                 
                 # Inicialización shinyjs
                 useShinyjs(),
